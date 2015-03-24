a81bcf06add1f8ac7a4cb63cf6982c477e00bf82
orientman <orientman@gmail.com>
2015-03-24 17:48:26 +0100

    Drawing done

---
+++ Drawing.fs
@@ -0,0 +1,69 @@
+﻿module Drawing
+
+open System.Drawing
+open Parser
+
+let draw (scale : int) (rep : string []) = 
+    let scaleF = float32 (scale)
+    let width = rep.[0].Length / 2 + 1
+    let scaledWidth = scale * width
+    let height = rep.Length
+    let scaledHeight = scale * height
+    let bitmap = new Bitmap(scaledWidth, scaledHeight)
+
+    use gr = Graphics.FromImage(bitmap)
+    gr.SmoothingMode <- Drawing2D.SmoothingMode.AntiAlias
+    gr.Clear(Color.White)
+
+    let drawGrid() = 
+        let grayPen = new Pen(Color.LightGray, 1.0f)
+        for i in 0..height do
+            gr.DrawLine(grayPen, new Point(0, scale * i), new Point(scaledWidth, scale * i))
+        for i in 0..width do
+            gr.DrawLine(grayPen, new Point(scale * i, 0), new Point(scale * i, scaledHeight))
+        gr.DrawRectangle(grayPen, 0, 0, scaledWidth - 1, scaledHeight - 1)
+    
+    let drawShape shape = 
+        let pen = new Pen(Color.Black, scaleF)
+        let brush = new SolidBrush(Color.Black)
+        
+        let drawPixel p = 
+            let x, y = p
+            gr.FillRectangle(brush, scale * x, scale * y, scale, scale)
+        
+        let scalePoint (x, y) = 
+            (scaleF * (0.5f + float32 (x)), scaleF * (0.5f + float32 (y)))
+
+        let toPointF (x, y) = new PointF(x, y)
+
+        match shape with
+        | Free(points) -> 
+            let scaledPoints = 
+                points
+                |> List.map scalePoint
+                |> List.map toPointF
+                |> Array.ofList
+            gr.DrawPolygon(pen, scaledPoints)
+            gr.FillPolygon(brush, scaledPoints)
+        | Line(p1, p2) -> 
+            drawPixel p1
+            drawPixel p2
+            gr.DrawLine(pen, p1 |> scalePoint |> toPointF, p2 |> scalePoint |> toPointF)
+        | Pixel(p) -> drawPixel p
+        | Ellipse(p1, p2, p3, p4) -> 
+            let points = [ p1; p2; p3; p4 ]
+            let minX = points |> List.map fst |> List.min
+            let maxX = points |> List.map fst |> List.max
+            let minY = points |> List.map snd |> List.min
+            let maxY = points |> List.map snd |> List.max
+            let x, y = (minX, minY) |> scalePoint
+            let width, height = (maxX - minX, maxY - minY) |> scalePoint
+            gr.DrawEllipse(pen, x, y, width, height)
+            gr.FillEllipse(brush, x, y, width, height)
+    
+    if scale > 1 then drawGrid()
+    rep
+    |> parse
+    |> List.iter drawShape
+    bitmap
+
--- Playground.fsx
+++ Playground.fsx
@@ -1,80 +1,25 @@
 ﻿// http://cocoamine.net/blog/2015/03/20/replacing-photoshop-with-nsstring/
 open System
-open System.Drawing
 open System.Windows.Forms
 
 #load "Parser.fs"
 #load "Shapes.fs"
-open Parser
+#load "Drawing.fs"
+
+open Drawing
 
 let mainForm = new Form(Width = 400, Height = 400, Text = "AsciiImage")
-let menu = new ToolStrip()
-let btnSave = new ToolStripButton("Save", Enabled = false)
-
-ignore (menu.Items.Add(btnSave))
-
 let boxImage = 
     new PictureBox(BackColor = Color.White, Dock = DockStyle.Fill, 
                    SizeMode = PictureBoxSizeMode.CenterImage)
 
-mainForm.Controls.Add(menu)
 mainForm.Controls.Add(boxImage)
 
-// chevron is defined by 3 points, the angle is always 90 degrees
-// 
-// A 
-//   # 
-//     # 
-//       B 
-//     # 
-//   # 
-// C 
-let drawChevron (bounds : Rectangle) = 
-    let chevron = new Bitmap(bounds.Width - 10, bounds.Height - 10)
-    let rightMargin = 12.0f
-    let chevronHeight = 150.0f // then chevronWidth = chevronHeight/2
-    let lineWidth = 10.0f
-    let middle = 
-        new PointF(float32 (chevron.Width) - rightMargin - lineWidth / 2.0f, 
-                   float32 (chevron.Height) / 2.0f)
-    let mutable top = middle
-    top.X <- top.X - chevronHeight / 2.0f
-    top.Y <- top.Y + chevronHeight / 2.0f
-    let mutable bottom = top
-    bottom.Y <- bottom.Y - chevronHeight
-    use gr = Graphics.FromImage(chevron)
-    gr.Clear(Color.White)
-    let pen = new Pen(Color.Gray, lineWidth)
-    gr.DrawLines(pen, [| top; middle; bottom |])
-    chevron
+//let image = Shapes.``chevron + 2 shapes`` |> draw 30
+//let image = Shapes.``8 lines`` |> draw 30
+let image = Shapes.``ellipse + 2 pixels`` |> draw 30
+//let image = Shapes.``6 lines & free shape`` |> draw 30
 
-let draw (bounds : Rectangle) (shapes : Shape list) = 
-    let margin = 10
-    let scale = 20.0f
-    let lineWidth = scale * 1.0f
-
-    let bitmap = new Bitmap(bounds.Width - margin, bounds.Height - margin)
-    use gr = Graphics.FromImage(bitmap)
-    gr.Clear(Color.White)
-    let pen = new Pen(Color.Black, lineWidth)
-    let brush = new SolidBrush(Color.Black);
-
-    let drawShape (shape : Shape) =
-        let toPointF (x, y) = new PointF(scale * float32(x), scale * float32(y))
-        match shape with
-        | Free(points) -> gr.FillPolygon(brush, points |> List.map toPointF |> Array.ofList)
-        | Line(p1, p2) -> gr.DrawLine(pen, toPointF p1, toPointF p2)
-        | Pixel(p) -> ()
-        | Ellipse(p1, p2, p3, p4) -> ()
-
-    shapes |> List.iter drawShape
-    bitmap
-
-//let dotsSorted = getOrderedDots chevron
-//parse chevron
-//parse imageWith3Shapes
-
-boxImage.Image <- draw boxImage.Bounds (Shapes.``6 lines & free shape`` |> parse)
-
+boxImage.Image <- image
 [<STAThread>]
 do mainForm.Show()
