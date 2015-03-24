b1c35d052058debca79acd00da066306230b98c6
orientman <orientman@gmail.com>
2015-03-24 14:59:32 +0100

    Refactoring + tests

---
+++ Parser.fs
@@ -0,0 +1,56 @@
+﻿module Parser
+
+type Dot = int * int
+type Shape = 
+    | Free of Dot list
+    | Line of Dot * Dot
+    | Pixel of Dot
+    | Ellipse of Dot * Dot * Dot * Dot
+
+let private symbolsOrdered = 
+    seq { 
+        yield '0'
+        yield! [| '1'..'9' |]
+        yield! [| 'A'..'Z' |]
+        yield! [| 'a'..'z' |]
+    }
+    |> Array.ofSeq
+
+let getOrderedDots (arr : string []) = 
+    let indexOfSymbol (_, idx) = idx
+    seq { 
+        for y in 0..arr.Length - 1 do
+            let row = arr.[y].Replace(" ", "")
+            for x in 0..row.Length - 1 do
+                let elem = row.[x]
+                match symbolsOrdered |> Array.tryFindIndex (elem |> (=)) with
+                | Some idx -> yield ((x, y), idx)
+                | _ -> ()
+    } |> Seq.sortBy indexOfSymbol |> List.ofSeq
+
+let rec private parseDots acc dots =
+    let handleOrdered = function
+        | [] -> []
+        | single::[] -> [Pixel(single)]
+        | many -> [Free(many |> List.rev)]
+    seq {
+        match dots with
+        | (d1, i1)::(d2, i2)::tail when i2 = i1 + 1 ->
+            yield! parseDots (d1::acc) ((d2, i2)::tail)
+        | (d1, i1)::(d2, i2)::(d3, i3)::(d4, i4)::tail
+            when i1 = i2 && i2 = i3 && i3 = i4 ->
+            yield! handleOrdered acc
+            yield Ellipse(d1, d2, d3, d4)
+            yield! parseDots [] tail
+        | (d1, i1)::(d2, i2)::tail when i1 = i2 ->
+            yield! handleOrdered acc
+            yield Line(d1, d2)
+            yield! parseDots [] tail
+        | (d, _)::tail ->
+            yield! handleOrdered (d::acc)
+            yield! parseDots [] tail
+        | _ -> yield! handleOrdered acc
+    }
+
+let parse rep = rep |> getOrderedDots |> parseDots [] |> List.ofSeq
+
---
+++ ParserTests.fs
@@ -0,0 +1,91 @@
+﻿module ParserTests
+
+open NUnit.Framework
+open Swensen.Unquote
+open Parser
+
+[<Test>]
+let empty() =
+    let actual = Shapes.empty |> parse
+    let (expected:Shape list) = []
+
+    test <@ expected = actual @>
+
+[<Test>]
+let chevron() =
+    let actual = Shapes.chevron |> parse
+    let expected =
+        [ Free [ (2, 1)
+                 (3, 1)
+                 (7, 5)
+                 (7, 6)
+                 (3, 10)
+                 (2, 10)
+                 (2, 9)
+                 (5, 6)
+                 (5, 5)
+                 (2, 2) ] ]
+
+    test <@ expected = actual @>
+
+[<Test>]
+let ``chevron + 2 shapes``() =
+    let actual = Shapes.``chevron + 2 shapes`` |> parse
+    let expected = 
+        [ Free [ (3, 1)
+                 (4, 1)
+                 (8, 5)
+                 (8, 6)
+                 (4, 10)
+                 (3, 10)
+                 (3, 9)
+                 (6, 6)
+                 (6, 5)
+                 (3, 2) ]
+          Free [ (7, 0)
+                 (10, 3)
+                 (10, 0) ]
+          Free [ (0, 4)
+                 (2, 4)
+                 (2, 6)
+                 (3, 6)
+                 (3, 7)
+                 (0, 7) ] ]
+
+    test <@ expected = actual @>
+
+[<Test>]
+let ``8 lines``() =
+    let actual = Shapes.``8 lines`` |> parse
+    let expected = 
+        [ Line (( 0,  0), (11,  0))
+          Line ((11,  1), (11, 10))
+          Line (( 0, 11), (11, 11))
+          Line (( 0,  1), ( 0, 10))
+          Line (( 2,  2), ( 9,  2))
+          Line (( 2,  4), ( 9,  4))
+          Line (( 2,  6), ( 9,  6))
+          Line (( 4,  9), ( 7,  9)) ]
+
+    test <@ expected = actual @>
+
+[<Test>]
+let ``lines & free shape``() =
+    let actual = Shapes.``6 lines & free shape`` |> parse
+    let expected = 
+        [ Line (( 0,  0), (11,  0))
+          Line ((11,  1), (11, 10))
+          Line (( 0, 11), (11, 11))
+          Line (( 0,  1), ( 0, 10))
+          Line (( 2,  2), ( 3,  2))
+          Line (( 8,  2), ( 9,  2))
+          Free [(2, 4); (4, 4); (4, 6); (7, 6); (7, 4); (9, 4); (9, 9); (2, 9)] ]
+
+    test <@ expected = actual @>
+
+[<Test>]
+let ``ellipse + 2 pixels``() =
+    let actual = Shapes.``ellipse + 2 pixels`` |> parse
+    let expected = [Ellipse ((5, 2),(1, 6),(9, 6),(5, 10)); Pixel (1, 1); Pixel (9, 1)]
+
+    test <@ expected = actual @>
