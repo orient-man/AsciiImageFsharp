017349373011960989a58316098c47b94bc7f06b
orientman <orientman@gmail.com>
2015-03-25 18:54:32 +0100

    No need for seq

--- Parser.fs
+++ Parser.fs
@@ -13,41 +13,37 @@ let getOrderedDots (arr : string []) =
     let (|Solid|_|) = tryFindIndex [| 'A'..'Z' |]
     let (|Transparent|_|) = tryFindIndex [| 'a'..'z' |]
 
-    seq { 
-        for y in 0..arr.Length - 1 do
-            let row = arr.[y].Replace(" ", "")
-            for x in 0..row.Length - 1 do
-                match row.[x] with
-                | Solid idx  -> yield (((x, y), Solid), idx)
-                | Transparent idx -> yield (((x, y), Transparent), idx)
-                | _ -> ()
-    } |> Seq.sortBy snd |> List.ofSeq
+    [ for y in 0..arr.Length - 1 do
+          let row = arr.[y].Replace(" ", "")
+          for x in 0..row.Length - 1 do
+              match row.[x] with
+              | Solid idx -> yield (((x, y), Solid), idx)
+              | Transparent idx -> yield (((x, y), Transparent), idx)
+              | _ -> () ]
+    |> List.sortBy snd
 
-let rec private parseDots acc dots =
+let rec private parseDots acc dots = 
     let handleOrdered points =
         match points with
         | [] -> []
         | (p, opacity)::[] -> [opacity (Pixel(p))]
         | (_, opacity)::_ -> [opacity (Polygon(points |> List.map fst |> List.rev))]
 
-    seq {
-        match dots with
-        | (d1, i1)::(d2, i2)::tail when i2 = i1 + 1 ->
-            yield! parseDots (d1::acc) ((d2, i2)::tail)
-        | ((p1, opacity), i1)::(d2, i2)::(d3, i3)::(d4, i4)::tail
-            when i1 = i2 && i2 = i3 && i3 = i4 ->
-            yield! handleOrdered acc
-            yield opacity (Ellipse(p1, fst d2, fst d3, fst d4))
-            yield! parseDots [] tail
-        | ((p1, opacity), i1)::(d2, i2)::tail when i1 = i2 ->
-            yield! handleOrdered acc
-            yield opacity (Line(p1, fst d2))
-            yield! parseDots [] tail
-        | (d, _)::tail ->
-            yield! handleOrdered (d::acc)
-            yield! parseDots [] tail
-        | _ -> yield! handleOrdered acc
-    }
+    [ match dots with
+      | (d1, i1)::(d2, i2)::tail when i2 = i1 + 1 -> 
+          yield! parseDots (d1::acc) ((d2, i2)::tail)
+      | ((p1, opacity), i1)::(d2, i2)::(d3, i3)::(d4, i4)::tail
+        when i1 = i2 && i2 = i3 && i3 = i4 -> 
+          yield! handleOrdered acc
+          yield opacity (Ellipse(p1, fst d2, fst d3, fst d4))
+          yield! parseDots [] tail
+      | ((p1, opacity), i1)::(d2, i2)::tail when i1 = i2 ->
+          yield! handleOrdered acc
+          yield opacity (Line(p1, fst d2))
+          yield! parseDots [] tail
+      | (d, _)::tail ->
+          yield! handleOrdered (d::acc)
+          yield! parseDots [] tail
+      | _ -> yield! handleOrdered acc ]
 
-let parse rep = rep |> getOrderedDots |> parseDots [] |> List.ofSeq
-
+let parse rep = rep |> getOrderedDots |> parseDots []
